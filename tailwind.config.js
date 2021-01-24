7849f6254ae87547df0020b44fef6e9ab4a25052
Raphi W <ditshej@gmail.com>
2021-01-24 13:06:26 +0100

    add font, do some styling practice

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,24 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
+
 module.exports = {
-	purge: [
-		'./index.html',
-		'./js/**/*.js',
-	],
-	darkMode: false, // or 'media' or 'class'
-	theme: {
-		extend: {},
-	},
-	variants: {
-		extend: {},
-	},
-	plugins: [],
+  purge: [
+    './index.html',
+    './js/**/*.js',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Sniglet', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        lime: colors.lime,
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 };
