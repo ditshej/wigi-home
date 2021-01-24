11f913ce352f85efc466ad1ce473633303179d2f
Raphi W <ditshej@gmail.com>
2021-01-24 11:01:42 +0100

    lets try with https://laravel-mix.com/extensions/tailwindcss ... still not working... I totally feel like a noob 🙉

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,8 @@
 let mix = require('laravel-mix');
+require('mix-tailwindcss');
 
 mix
 	.js('js/main.js', '')
-	.postCss('css/main.css', '', [
-		require('tailwindcss'),
-	])
+	.postCss('css/main.css', '')
+	.tailwind()
 	.setPublicPath('dist');
