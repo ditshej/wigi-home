d61f3f16a69d12ad371996fe83372d53be341524
Raphi W <ditshej@gmail.com>
2021-03-22 16:52:35 +0100

    add some gallery placeholder

--- index.php
+++ index.php
@@ -379,11 +379,33 @@ require_once 'Config.php';
         </div>
       </div>
     </div>
-
   </section>
 
   <section id="gallery">
-
+    <div class="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
+      <ul class="grid grid-cols-1 gap-6 lg:grid-cols-2">
+        <li class="rounded-xl shadow group bg-gradient-to-br overflow-hidden hover:shadow-xl">
+          <img class="object-cover lg:h-full lg:w-full" src="https://source.unsplash.com/i96EPuuuzSo/1600x900"
+               alt="placeholder"/>
+        </li>
+        <li class="rounded-xl shadow group bg-gradient-to-br overflow-hidden hover:shadow-xl">
+          <img class="object-cover lg:h-full lg:w-full" src="https://source.unsplash.com/j2X6eIActf4/1600x900"
+               alt="placeholder"/>
+        </li>
+        <li class="rounded-xl shadow group bg-gradient-to-br overflow-hidden hover:shadow-xl">
+          <img class="object-cover lg:h-full lg:w-full" src="https://source.unsplash.com/1P6whXqXNBc/1600x900"
+               alt="placeholder"/>
+        </li>
+        <li class="rounded-xl shadow group bg-gradient-to-br overflow-hidden hover:shadow-xl">
+          <img class="object-cover lg:h-full lg:w-full" src="https://source.unsplash.com/O--XbrLh3g0/1600x900"
+               alt="placeholder"/>
+        </li>
+        <li class="rounded-xl shadow group bg-gradient-to-br overflow-hidden hover:shadow-xl">
+          <img class="object-cover lg:h-full lg:w-full" src="https://source.unsplash.com/Bw-Hm2ya8aA/1600x900"
+               alt="placeholder"/>
+        </li>
+      </ul>
+    </div>
   </section>
 </main>
 
