0d42af8b29077a9ca6af65a9ae92604f4d48a1d7
Raphi W <ditshej@gmail.com>
2021-04-16 11:51:59 +0200

    put to-mail-address to env

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -18,11 +18,11 @@ class ContactFormController extends Controller
             'date_from' => 'required|date|after:tomorrow',
             'time_from' => 'nullable|date_format:H:i',
             'date_to' => 'required|date|after:date_from',
-            'time_to' => 'nullable|required_with:time_from|date_format:H:i|different:time_from',
+            'time_to' => 'nullable|required_with:time_from|date_format:H:i',
             'how_can_we_help' => 'nullable',
             'socks' => 'nullable|boolean',
         ]);
-        Mail::to('ditshej@gmail.com')->send(new Contact($contact_form_data));
+        Mail::to(env('MAIL_TO_ADDRESS'))->send(new Contact($contact_form_data));
         return view('contact_response_message');
     }
 }
