bada6568ebf448cb3a6887b53abf66f731c7c060
Raphi W <ditshej@gmail.com>
2021-11-23 14:12:23 +0100

    introduce thumbnails in webp

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,31 +2,31 @@ require('./bootstrap');
 
 import 'alpinejs';
 
-import {Calendar} from '@fullcalendar/core';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import googleCalendarPlugin from '@fullcalendar/google-calendar';
+// import {Calendar} from '@fullcalendar/core';
+// import dayGridPlugin from '@fullcalendar/daygrid';
+// import googleCalendarPlugin from '@fullcalendar/google-calendar';
 import glightbox from "glightbox/src/js/glightbox";
 
 document.addEventListener('DOMContentLoaded', function () {
-    let calendarEl = document.getElementById('calendar-container');
-    const calendar = new Calendar(calendarEl, {
-        plugins: [dayGridPlugin, googleCalendarPlugin],
-        initialView: 'dayGridMonth',
-        headerToolbar: {
-            left: 'prev,next today',
-            center: 'title',
-            right: ''
-        },
-        googleCalendarApiKey: process.env.MIX_GOOGLE_CALENDAR_API_KEY,
-        events: {
-            googleCalendarId: process.env.MIX_GOOGLE_CALENDAR_ID
-        }
-    });
-    calendar.render();
+    // let calendarEl = document.getElementById('calendar-container');
+    // const calendar = new Calendar(calendarEl, {
+    //     plugins: [dayGridPlugin, googleCalendarPlugin],
+    //     initialView: 'dayGridMonth',
+    //     headerToolbar: {
+    //         left: 'prev,next today',
+    //         center: 'title',
+    //         right: ''
+    //     },
+    //     googleCalendarApiKey: process.env.MIX_GOOGLE_CALENDAR_API_KEY,
+    //     events: {
+    //         googleCalendarId: process.env.MIX_GOOGLE_CALENDAR_ID
+    //     }
+    // });
+    // calendar.render();
 
     const gallery = glightbox({
         touchNavigation: true,
         loop: true,
         autoplayVideos: true,
-    })
+    });
 });
